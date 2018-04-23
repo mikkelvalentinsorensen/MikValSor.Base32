f36e45fab9fb0543bb19b4576ca56bd9906045e5
Mikkel Valentin Sørensen <mvs@codehouse.com>
2018-04-23 12:31:25 +0200

    Added Unit test project and simple tests

---
+++ source/MikValSor.Base32.Test/Base32Decoder.cs
@@ -0,0 +1,248 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace MikValSor.Base32.Test
+{
+	[TestFixture]
+	public class Base32Decoder
+	{
+		[Test]
+		public void DecodeTest_Empty_Empty()
+		{
+			//Arrange
+			var base32 = String.Empty;
+
+			//Act
+			var actual = MikValSor.Encoding.Base32Decoder.Decode(base32);
+
+			//Assert
+			var expected = new byte[0];
+			Assert.AreEqual(expected, actual);
+		}
+
+		[Test]
+		public void DecodeTest_AA_000()
+		{
+			//Arrange
+			var base32 = "AA======";
+
+			//Act
+			var actual = MikValSor.Encoding.Base32Decoder.Decode(base32);
+
+			//Assert
+			var expected = new byte[] { 000 };
+			Assert.AreEqual(expected, actual);
+		}
+
+		[Test]
+		public void DecodeTest_AE_001()
+		{
+			//Arrange
+			var base32 = "AE======";
+
+			//Act
+			var actual = MikValSor.Encoding.Base32Decoder.Decode(base32);
+
+			//Assert
+			var expected = new byte[] { 001 };
+			Assert.AreEqual(expected, actual);
+		}
+
+		[Test]
+		public void DecodeTest_AI_002()
+		{
+			//Arrange
+			var base32 = "AI======";
+
+			//Act
+			var actual = MikValSor.Encoding.Base32Decoder.Decode(base32);
+
+			//Assert
+			var expected = new byte[] { 002 };
+			Assert.AreEqual(expected, actual);
+		}
+
+		[Test]
+		public void DecodeTest_AM_003()
+		{
+			//Arrange
+			var base32 = "AM======";
+
+			//Act
+			var actual = MikValSor.Encoding.Base32Decoder.Decode(base32);
+
+			//Assert
+			var expected = new byte[] { 003 };
+			Assert.AreEqual(expected, actual);
+		}
+
+		[Test]
+		public void DecodeTest_AQ_004()
+		{
+			//Arrange
+			var base32 = "AQ======";
+
+			//Act
+			var actual = MikValSor.Encoding.Base32Decoder.Decode(base32);
+
+			//Assert
+			var expected = new byte[] { 004 };
+			Assert.AreEqual(expected, actual);
+		}
+
+		[Test]
+		public void DecodeTest_AU_005()
+		{
+			//Arrange
+			var base32 = "AU======";
+
+			//Act
+			var actual = MikValSor.Encoding.Base32Decoder.Decode(base32);
+
+			//Assert
+			var expected = new byte[] { 005 };
+			Assert.AreEqual(expected, actual);
+		}
+
+		[Test]
+		public void DecodeTest_AY_006()
+		{
+			//Arrange
+			var base32 = "AY======";
+
+			//Act
+			var actual = MikValSor.Encoding.Base32Decoder.Decode(base32);
+
+			//Assert
+			var expected = new byte[] { 006 };
+			Assert.AreEqual(expected, actual);
+		}
+
+		[Test]
+		public void DecodeTest_A4_007()
+		{
+			//Arrange
+			var base32 = "A4======";
+
+			//Act
+			var actual = MikValSor.Encoding.Base32Decoder.Decode(base32);
+
+			//Assert
+			var expected = new byte[] { 007 };
+			Assert.AreEqual(expected, actual);
+		}
+
+		[Test]
+		public void DecodeTest_BA_008()
+		{
+			//Arrange
+			var base32 = "BA======";
+
+			//Act
+			var actual = MikValSor.Encoding.Base32Decoder.Decode(base32);
+
+			//Assert
+			var expected = new byte[] { 008 };
+			Assert.AreEqual(expected, actual);
+		}
+
+		[Test]
+		public void DecodeTest_BE_009()
+		{
+			//Arrange
+			var base32 = "BE======";
+
+			//Act
+			var actual = MikValSor.Encoding.Base32Decoder.Decode(base32);
+
+			//Assert
+			var expected = new byte[] { 009 };
+			Assert.AreEqual(expected, actual);
+		}
+
+		[Test]
+		public void DecodeTest_BI_010()
+		{
+			//Arrange
+			var base32 = "BI======";
+
+			//Act
+			var actual = MikValSor.Encoding.Base32Decoder.Decode(base32);
+
+			//Assert
+			var expected = new byte[] { 010 };
+			Assert.AreEqual(expected, actual);
+		}
+
+
+		[Test]
+		public void DecodeTest_AAAA_000_000()
+		{
+			//Arrange
+			var base32 = "AAAA====";
+
+			//Act
+			var actual = MikValSor.Encoding.Base32Decoder.Decode(base32);
+
+			//Assert
+			var expected = new byte[] { 000, 000 };
+			Assert.AreEqual(expected, actual);
+		}
+
+		[Test]
+		public void DecodeTest_AAAAA_000_000_000()
+		{
+			//Arrange
+			var base32 = "AAAAA===";
+
+			//Act
+			var actual = MikValSor.Encoding.Base32Decoder.Decode(base32);
+
+			//Assert
+			var expected = new byte[] { 000, 000, 000 };
+			Assert.AreEqual(expected, actual);
+		}
+
+		[Test]
+		public void DecodeTest_AAAAAAA_000_000_000_000()
+		{
+			//Arrange
+			var base32 = "AAAAAAA=";
+
+			//Act
+			var actual = MikValSor.Encoding.Base32Decoder.Decode(base32);
+
+			//Assert
+			var expected = new byte[] { 000, 000, 000, 000 };
+			Assert.AreEqual(expected, actual);
+		}
+
+		[Test]
+		public void DecodeTest_AAAAAAAA_000_000_000_000_000()
+		{
+			//Arrange
+			var base32 = "AAAAAAAA";
+
+			//Act
+			var actual = MikValSor.Encoding.Base32Decoder.Decode(base32);
+
+			//Assert
+			var expected = new byte[] { 000, 000, 000, 000, 000 };
+			Assert.AreEqual(expected, actual);
+		}
+
+		[Test]
+		public void DecodeTest_AAAAAAAAAA_000_000_000_000_000_000()
+		{
+			//Arrange
+			var base32 = "AAAAAAAAAA======";
+
+			//Act
+			var actual = MikValSor.Encoding.Base32Decoder.Decode(base32);
+
+			//Assert
+			var expected = new byte[] { 000, 000, 000, 000, 000, 000 };
+			Assert.AreEqual(expected, actual);
+		}
+	}
+}
---
+++ source/MikValSor.Base32.Test/Base32Encoder.cs
@@ -0,0 +1,247 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace MikValSor.Base32.Test
+{
+	[TestFixture]
+	public class Base32Encoder
+	{
+		[Test]
+		public void EncodeTest_Empty_Empty()
+		{
+			//Arrange
+			var bytearray = new byte[] {};
+
+			//Act
+			var actual = MikValSor.Encoding.Base32Encoder.Encode(bytearray);
+
+			//Assert
+			var expected = String.Empty;
+			Assert.AreEqual(expected, actual);
+		}
+
+		[Test]
+		public void EncodeTest_000_AA()
+		{
+			//Arrange
+			var bytearray = new byte[] { 000 };
+
+			//Act
+			var actual = MikValSor.Encoding.Base32Encoder.Encode(bytearray);
+
+			//Assert
+			var expected = "AA======";
+			Assert.AreEqual(expected, actual);
+		}
+
+		[Test]
+		public void EncodeTest_001_AE()
+		{
+			//Arrange
+			var bytearray = new byte[] { 001 };
+
+			//Act
+			var actual = MikValSor.Encoding.Base32Encoder.Encode(bytearray);
+
+			//Assert
+			var expected = "AE======";
+			Assert.AreEqual(expected, actual);
+		}
+
+		[Test]
+		public void EncodeTest_002_AI()
+		{
+			//Arrange
+			var bytearray = new byte[] { 002 };
+
+			//Act
+			var actual = MikValSor.Encoding.Base32Encoder.Encode(bytearray);
+
+			//Assert
+			var expected = "AI======";
+			Assert.AreEqual(expected, actual);
+		}
+
+		[Test]
+		public void EncodeTest_003_AM()
+		{
+			//Arrange
+			var bytearray = new byte[] { 003 };
+
+			//Act
+			var actual = MikValSor.Encoding.Base32Encoder.Encode(bytearray);
+
+			//Assert
+			var expected = "AM======";
+			Assert.AreEqual(expected, actual);
+		}
+
+		[Test]
+		public void EncodeTest_004_AQ()
+		{
+			//Arrange
+			var bytearray = new byte[] { 004 };
+
+			//Act
+			var actual = MikValSor.Encoding.Base32Encoder.Encode(bytearray);
+
+			//Assert
+			var expected = "AQ======";
+			Assert.AreEqual(expected, actual);
+		}
+
+		[Test]
+		public void EncodeTest_005_AU()
+		{
+			//Arrange
+			var bytearray = new byte[] { 005 };
+
+			//Act
+			var actual = MikValSor.Encoding.Base32Encoder.Encode(bytearray);
+
+			//Assert
+			var expected = "AU======";
+			Assert.AreEqual(expected, actual);
+		}
+
+		[Test]
+		public void EncodeTest_006_AY()
+		{
+			//Arrange
+			var bytearray = new byte[] { 006 };
+
+			//Act
+			var actual = MikValSor.Encoding.Base32Encoder.Encode(bytearray);
+
+			//Assert
+			var expected = "AY======";
+			Assert.AreEqual(expected, actual);
+		}
+
+		[Test]
+		public void EncodeTest_007_A4()
+		{
+			//Arrange
+			var bytearray = new byte[] { 007 };
+
+			//Act
+			var actual = MikValSor.Encoding.Base32Encoder.Encode(bytearray);
+
+			//Assert
+			var expected = "A4======";
+			Assert.AreEqual(expected, actual);
+		}
+
+		[Test]
+		public void EncodeTest_008_BA()
+		{
+			//Arrange
+			var bytearray = new byte[] { 008 };
+
+			//Act
+			var actual = MikValSor.Encoding.Base32Encoder.Encode(bytearray);
+
+			//Assert
+			var expected = "BA======";
+			Assert.AreEqual(expected, actual);
+		}
+
+		[Test]
+		public void EncodeTest_009_BE()
+		{
+			//Arrange
+			var bytearray = new byte[] { 009 };
+
+			//Act
+			var actual = MikValSor.Encoding.Base32Encoder.Encode(bytearray);
+
+			//Assert
+			var expected = "BE======";
+			Assert.AreEqual(expected, actual);
+		}
+
+		[Test]
+		public void EncodeTest_010_BI()
+		{
+			//Arrange
+			var bytearray = new byte[] { 010 };
+
+			//Act
+			var actual = MikValSor.Encoding.Base32Encoder.Encode(bytearray);
+
+			//Assert
+			var expected = "BI======";
+			Assert.AreEqual(expected, actual);
+		}
+
+		[Test]
+		public void EncodeTest_000_000_AAAA()
+		{
+			//Arrange
+			var bytearray = new byte[] { 000, 000 };
+
+			//Act
+			var actual = MikValSor.Encoding.Base32Encoder.Encode(bytearray);
+
+			//Assert
+			var expected = "AAAA====";
+			Assert.AreEqual(expected, actual);
+		}
+
+		[Test]
+		public void EncodeTest_000_000_000_AAAAA()
+		{
+			//Arrange
+			var bytearray = new byte[] { 000, 000, 000 };
+
+			//Act
+			var actual = MikValSor.Encoding.Base32Encoder.Encode(bytearray);
+
+			//Assert
+			var expected = "AAAAA===";
+			Assert.AreEqual(expected, actual);
+		}
+
+		[Test]
+		public void EncodeTest_000_000_000_000_AAAAAAA()
+		{
+			//Arrange
+			var bytearray = new byte[] { 000, 000, 000, 000 };
+
+			//Act
+			var actual = MikValSor.Encoding.Base32Encoder.Encode(bytearray);
+
+			//Assert
+			var expected = "AAAAAAA=";
+			Assert.AreEqual(expected, actual);
+		}
+
+		[Test]
+		public void EncodeTest_000_000_000_000_000_AAAAAAAA()
+		{
+			//Arrange
+			var bytearray = new byte[] { 000, 000, 000, 000, 000 };
+
+			//Act
+			var actual = MikValSor.Encoding.Base32Encoder.Encode(bytearray);
+
+			//Assert
+			var expected = "AAAAAAAA";
+			Assert.AreEqual(expected, actual);
+		}
+
+		[Test]
+		public void EncodeTest_000_000_000_000_000_000_AAAAAAAAAA()
+		{
+			//Arrange
+			var bytearray = new byte[] { 000, 000, 000, 000, 000, 000 };
+
+			//Act
+			var actual = MikValSor.Encoding.Base32Encoder.Encode(bytearray);
+
+			//Assert
+			var expected = "AAAAAAAAAA======";
+			Assert.AreEqual(expected, actual);
+		}
+	}
+}
